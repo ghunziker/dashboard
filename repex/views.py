2c40e007a6a043bfdc3e8749a0b56a2294ef7361
ghunziker <77299407+ghunziker@users.noreply.github.com>
2023-02-25 14:34:10 +0100

    Initial commit

---
+++ repex/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect, reverse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+ 
+# Views
